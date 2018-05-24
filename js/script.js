6254b33a61417ae0b3006c5f6b7a613b413c1e62
IlonaArt <ilonasurdream@gmail.com>
2018-05-25 00:05:26 +0300

    Fixed sliders

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var btnClose = document.querySelector(".modal-close");
 var radioButton = document.querySelectorAll(".radio-button");
 var modalOverlay = document.querySelector(".modal-overlay");
 var sliderItems = document.querySelectorAll(".slider-item");
+var body = document.querySelector("body");
 
 btnFeedback.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -25,13 +26,17 @@ for (var i = 0; i < radioButton.length; i++) {
 		sliderItems.forEach(function(item) {
 			item.classList.remove("active");
 		});
+		body.classList.remove("switch2");
+		body.classList.remove("switch3");
 		this.classList.add("active");
 		if(this.classList.contains("one")) {
 			document.querySelector(".slider-item.one").classList.add("active");
 		} else if(this.classList.contains("two")) {
 			document.querySelector(".slider-item.two").classList.add("active");
+			body.classList.add("switch2");
 		} else if(this.classList.contains("three")) {
 			document.querySelector(".slider-item.three").classList.add("active");
+			body.classList.add("switch3");
 		}
 	})
 }
